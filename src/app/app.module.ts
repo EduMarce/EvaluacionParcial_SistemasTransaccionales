0147f650a0ca78cdb87cad09e188c6f22e97141d
Eduardo <edumarcer2@gmail.com>
2022-11-04 14:01:05 -0500

    Creación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NewEspacioComponent } from './new-espacio/new-espacio.component';
+import { ListEspaciosComponent } from './list-espacios/list-espacios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    NewEspacioComponent,
+    ListEspaciosComponent
   ],
   imports: [
     BrowserModule,
